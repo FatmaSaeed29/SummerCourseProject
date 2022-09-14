ce94556940872c15238c1f4517d8a840483f1e79
FatmaSaeed29 <fatmasaeedwasal@gmail.com>
2022-09-14 23:51:00 +0200

    Our Project

---
+++ Project/js/script1.js
@@ -0,0 +1,90 @@
+
+//var dt = new Date();
+//document.getElementById('date-time').innerHTML=dt;
+
+//selecting all required elements
+const dropArea = document.querySelector(".drop"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
+
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
+}
+
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drop to Upload File or Click Here";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+        // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+      // let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drop to Upload File or Click Here";
+  }
+}
+
+
+$(document).ready(function () { 
+ 
+  $("#repeatDivBtn").click(function () { 
+  
+   $newid = $(this).data("increment"); 
+   $repeatDiv = $("#repeatDiv").wrap('<div/>').parent().html(); 
+   $('#repeatDiv').unwrap(); 
+   $($repeatDiv).insertAfter($(".repeatDiv").last()); 
+   $(".repeatDiv").last().attr('id',   "repeatDiv" + '_' + $newid); 
+   $("#repeatDiv" + '_' + $newid).append('<div class="input-group-append"><i class="far fa-times ml-2 mt-2 removeDivBtn"  data-id="repeatDiv'+'_'+ $newid+'"></i></div>'); 
+   $newid++; 
+   $(this).data("increment", $newid); 
+  
+  }); 
+  
+  
+  $(document).on('click', '.removeDivBtn', function () { 
+  
+   $divId = $(this).data("id"); 
+   $("#"+$divId).remove(); 
+   $inc = $("#repeatDivBtn").data("increment"); 
+   $("#repeatDivBtn").data("increment", $inc-1); 
+  
+  }); 
+  
+ });
+ 
